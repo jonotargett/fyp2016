ed7da58f9d20c203e033cd583cf0aa99a2b712db
Harrison <hvince95@gmail.com>
2016-07-24 16:13:57 +0930

    Cleaning Code in VirtualPlatform

--- fyp2016/VirtualPlatform.cpp
+++ fyp2016/VirtualPlatform.cpp
@@ -18,25 +18,39 @@ bool VirtualPlatform::initialise(NavigationSystem* nav, SDL_Renderer* r) {
 	return true;
 }
 
+/*
+	Draws path to texture for the given drawScale and focus point defined within the function.
+*/
 void VirtualPlatform::drawTexture() {
 	texture->setAsRenderTarget();
 
 	//clear screen
-	SDL_SetRenderDrawColor(texture->getRenderer(), 0xFF, 0xFF, 0xFF, 0xFF);
-	SDL_RenderClear(texture->getRenderer());
+	SDL_SetRenderDrawColor(texture->getRenderer, 0xFF, 0xFF, 0xFF, 0xFF);
+	SDL_RenderClear(texture->getRenderer);
 
-	SDL_SetRenderDrawColor(texture->getRenderer(), 0xFF, 0xFF, 0x00, 0xFF);
-	int drawScale = 20;
-	int focusX = 45;
-	int focusY = 85;
+	int drawScale = 80;
+	int focusX = 60;
+	int focusY = 100;
+
 	for (int i = 0; i < (int)ns->getPath().size() - 1; i++) {
-		SDL_RenderDrawLine(texture->getRenderer(), ns->getPath().at(i)->x * drawScale - focusX*drawScale, ns->getPath().at(i)->y * drawScale * -1 + textureHeight + focusY*drawScale, ns->getPath().at(i+1)->x * drawScale - focusX*drawScale, ns->getPath().at(i+1)->y * drawScale * -1 + textureHeight + focusY*drawScale);
+
+		double x1Loc = ns->getPath().at(i)->x;
+		double y1Loc = ns->getPath().at(i)->y;
+		double x2Loc = ns->getPath().at(i + 1)->x;
+		double y2Loc = ns->getPath().at(i + 1)->y;
+
+		// transformed (x,y) locations for scale, computers inverted y coordinate, and focus point
+		double x1transform = x1Loc * drawScale - focusX*drawScale + textureWidth / 2;
+		double y1transform = y1Loc * drawScale * -1 + textureHeight + focusY*drawScale - textureHeight / 2;
+		double x2transform = x2Loc * drawScale - focusX*drawScale + textureWidth / 2;
+		double y2transform = y2Loc * drawScale * -1 + textureHeight + focusY*drawScale - textureHeight / 2;
+
+		SDL_SetRenderDrawColor(texture->getRenderer, 0xFF, 0xFF, 0x00, 0xFF);
+		SDL_RenderDrawLine(texture->getRenderer, (int)x1transform, (int)y1transform, (int)x2transform, (int)y2transform);
+		SDL_SetRenderDrawColor(texture->getRenderer, 0x00, 0x00, 0x00, 0xFF);
+		SDL_RenderDrawPoint(texture->getRenderer, (int)x1transform, (int)y1transform);
 	}
-	SDL_SetRenderDrawColor(texture->getRenderer(), 0x00, 0x00, 0x00, 0xFF);
-	for (int i = 0; i < (int)ns->getPath().size(); i++) {
-		SDL_RenderDrawPoint(texture->getRenderer(), ns->getPath().at(i)->x * drawScale - focusX*drawScale, ns->getPath().at(i)->y * drawScale * -1 + textureHeight + focusY*drawScale);
-	}
-	
+
 	/*
 	//render red filled quad
 	SDL_Rect fillRect = { 0, 0, 10, 10 };
@@ -45,7 +59,7 @@ void VirtualPlatform::drawTexture() {
 	*/
 
 	//reset render target
-	SDL_SetRenderTarget(texture->getRenderer(), NULL);
+	SDL_SetRenderTarget(texture->getRenderer, NULL);
 }
 
 
